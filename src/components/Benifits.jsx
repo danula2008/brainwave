5f6cd7ddf999ac4acb21f4b41880edc0e49e0440
danula2008 <danusathmin@gmail.com>
2024-06-18 18:54:27 +0530

    completed benifits

--- src/components/Benifits.jsx
+++ src/components/Benifits.jsx
@@ -1,8 +1,11 @@
-import Section from "./Section";
-import Heading from "./Heading";
 import { benefits } from "../constants";
+import Heading from "./Heading";
+import Section from "./Section";
+import Arrow from "../assets/svg/Arrow";
+import { GradientLight } from "./design/Benefits";
+import ClipPath from "../assets/svg/ClipPath";
 
-const Benifits = () => {
+const Benefits = () => {
   return (
     <Section id="features">
       <div className="container relative z-2">
@@ -24,9 +27,39 @@ const Benifits = () => {
                 <h5 className="h5 mb-5">{item.title}</h5>
                 <p className="body-2 mb-6 text-n-3">{item.text}</p>
                 <div className="flex items-center mt-auto">
-                  <img src={item.iconUrl} width={48} height={48} alt="" />
+                  <img
+                    src={item.iconUrl}
+                    width={48}
+                    height={48}
+                    alt={item.title}
+                  />
+                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
+                    Explore more
+                  </p>
+                  <Arrow />
                 </div>
               </div>
+
+              {item.light && <GradientLight />}
+
+              <div
+                className="absolute inset-0.5 bg-n-8"
+                style={{ clipPath: "url(#benefits)" }}
+              >
+                <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
+                  {item.imageUrl && (
+                    <img
+                      src={item.imageUrl}
+                      width={380}
+                      height={362}
+                      alt={item.title}
+                      className="w-full h-full object-cover"
+                    />
+                  )}
+                </div>
+              </div>
+
+              <ClipPath />
             </div>
           ))}
         </div>
@@ -35,4 +68,4 @@ const Benifits = () => {
   );
 };
 
-export default Benifits;
+export default Benefits;
